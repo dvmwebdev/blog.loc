be82d7d707f17e27cb839275753012839ac92e5f
dvmwebdev <dvmwebdev@gmail.com>
2023-02-21 22:17:12 +0200

    створено головний лейаут

---
+++ layouts/site.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <header>h</header>
+    <main>m</main>
+    <footer>f</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "site",
+};
+</script>
+
+<style scoped></style>
