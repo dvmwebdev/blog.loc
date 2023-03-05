5a8ade6d95062f2723889dd9297e653c506bcc84
dvmwebdev <dvmwebdev@gmail.com>
2023-03-05 21:48:59 +0200

    встановлено nuxt auth

--- components/auth/BLogin.vue
+++ components/auth/BLogin.vue
@@ -1,27 +1,39 @@
 <template>
-<div class="">
-  <b-form-group>
-    <b-form-input v-model="email" placeholder="Email"></b-form-input>
-  </b-form-group>
-  <b-form-group>
-  <b-form-input v-model="password" placeholder="Пароль"></b-form-input>
-  </b-form-group>
-  <b-button variant="primary">Війти</b-button>
-</div>
+  <div class="">
+    <form @submit.prevent="login">
+      <b-form-group>
+        <b-form-input v-model="email" placeholder="Email"></b-form-input>
+      </b-form-group>
+      <b-form-group>
+        <b-form-input v-model="password" placeholder="Пароль"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Війти</b-button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "BLogin",
-  data(){
+  data() {
     return {
-      email: '',
-      password: ''
-    }
-  }
-}
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        let res = await this.$auth.loginWith("laravelJWT", {
+          data: { email: this.email, password: this.password },
+        });
+        console.log(res.data);
+      } catch (e) {
+        console.log(e.response.data);
+      }
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
     "bootstrap-vue/nuxt",
   ],
 
@@ -43,6 +44,28 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: "http://localhost:8000/api/v1/",
+  },
+
+  auth: {
+    strategies: {
+      laravelJWT: {
+        provider: "laravel/jwt",
+        url: "http://localhost:8000/api/v1/",
+        endpoints: {
+          login: { url: "auth/login", method: "post" },
+          refresh: { url: "auth/refresh", method: "post" },
+          user: { url: "auth/user", method: "post" },
+          logout: { url: "auth/logout", method: "post" },
+        },
+        token: {
+          property: "access_token",
+          maxAge: 60 * 60,
+        },
+        refreshToken: {
+          maxAge: 20160 * 60,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
