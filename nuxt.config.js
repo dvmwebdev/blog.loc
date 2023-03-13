93a62d870a919cf98d0da5e84e59b6d1bb34a2ae
dvmwebdev <dvmwebdev@gmail.com>
2023-03-13 08:27:55 +0200

    видалено semantic ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/css/semantic/semantic.css", "~/assets/css/main.scss"],
+  css: ["~/assets/css/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
